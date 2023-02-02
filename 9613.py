f6668c6f298237ad48e03191a9d065842e68a0f2
Chanwoong1 <jcw7468@gmail.com>
2023-02-02 09:44:51 +0900

    feat: 9613

---
+++ 9613.py
@@ -0,0 +1,12 @@
+# GCD 합
+# https://www.acmicpc.net/problem/9613
+
+from itertools import combinations
+from math import gcd
+for _ in range(int(input())) :
+    lst = list(map(int, input().split()))
+    n = lst.pop(0)
+    answer = 0
+    for i, j in combinations(lst, 2) :
+        answer += gcd(i, j)
+    print(answer)
